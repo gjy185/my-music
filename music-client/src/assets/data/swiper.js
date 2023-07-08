938b2f2fe0865c02284c8cfe56efac2f56dcaa0c
gjy <2309957255@qq.com>
2023-07-08 11:51:04 +0800

    初始化

---
+++ music-client/src/assets/data/swiper.js
@@ -0,0 +1,43 @@
+// 轮播图
+// const swiperList = [{
+//   picImg: require('@/assets/img/swiper/blur-1851426_640.jpg')
+// }, {
+//   picImg: require('@/assets/img/swiper/concert-768722_640.jpg')
+// }, {
+//   picImg: require('@/assets/img/swiper/boy-984293_640.jpg')
+// }, {
+//   picImg: require('@/assets/img/swiper/ipad-605439_640.jpg')
+// }, {
+//   picImg: require('@/assets/img/swiper/microphone-1209816_640.jpg')
+// }, {
+//   picImg: require('@/assets/img/swiper/music-notes-3221097_640.jpg')
+// }, {
+//   picImg: require('@/assets/img/swiper/piano-1655558_640.jpg')
+// }, {
+//   picImg: require('@/assets/img/swiper/turntable-1337986_640.jpg')
+// }]
+
+// export {
+//   swiperList
+// }
+const swiperList = [{
+  picImg: require('@/assets/img/swiper/bgc1.jpg')
+}, {
+  picImg: require('@/assets/img/swiper/bgc2.jpg')
+}, {
+  picImg: require('@/assets/img/swiper/bgc3.jpg')
+}, {
+  picImg: require('@/assets/img/swiper/bgc4.jpg')
+}, {
+  picImg: require('@/assets/img/swiper/bgc5.jpg')
+}, {
+  picImg: require('@/assets/img/swiper/bgc6.jpg')
+}, {
+  picImg: require('@/assets/img/swiper/bgc7.jpg')
+}, {
+  picImg: require('@/assets/img/swiper/bgc8.jpg')
+}]
+
+export {
+  swiperList
+}
